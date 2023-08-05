d204378708d32b5e51fab110c5e3f53111517d24
Sinan Inel <sinan.inel@aalto.fi>
2023-08-05 10:32:50 +0300

    Fixing mistakes in initial setup.

--- docs/source/overview.rst
+++ docs/source/overview.rst
@@ -6,4 +6,4 @@ Overview
 
 Profkit is an open-source Python toolkit for using profilers with support for filtering, analysis and visualization.
 
-The goal of Profkit is to provide a common interface for different Python profilers, such as `Qiskit Dynamics <https://qiskit.org/ecosystem/dynamics/index.html>`_, `QuTiP <https://qutip.org/docs/latest/index.html>`_, and `C3 <https://c3-toolset.readthedocs.io/en/master/index.html>`_, as well as extending their functionality with respect to filtering, analysis, and visualization.
+The goal of Profkit is to provide a common interface for different Python profilers, such as `cprofile <https://docs.python.org/3/library/profile.html>`_, `Palanteer <https://dfeneyrou.github.io/palanteer/>`_, `pyinstrument <https://pyinstrument.readthedocs.io/en/latest/>`_, and `Yappi <https://github.com/sumerc/yappi/>`_, as well as unifying and extending their functionality with respect to filtering, analysis, and visualization.
---
+++ tests/profkit/debug_test.py
@@ -0,0 +1,28 @@
+#  ********************************************************************************
+#
+#    _________ __________ _
+#   / ___/ __ `/ ___/ __ `/    Python toolkit
+#  / /__/ /_/ (__  ) /_/ /     for control and analysis
+#  \___/\__,_/____/\__, /      of superconducting qubits
+#                    /_/
+#
+#  Copyright (c) 2023 Sinan Inel <sinan.inel@aalto.fi>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ********************************************************************************
+"""Tests used for debugging."""
+from __future__ import annotations
+
+
+def exclude_test_debug() -> None:
+    """Unit test for debugging."""
